95cf8e8fb9bf548b852e63df1c37ae953d3876f5
MrDan4es <mrdan4es@gmail.com>
2024-07-22 19:44:52 +0500

    added OTEL traceing

--- cmd/server.go
+++ cmd/server.go
@@ -1,27 +1,185 @@
 package main
 
 import (
+	"context"
+	"errors"
 	"fmt"
-	"log"
+	"math/rand/v2"
+	"net"
 	"net/http"
+	"os"
+	"os/signal"
+	"slices"
+	"time"
+
+	"go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp"
+	"go.opentelemetry.io/otel"
+	"go.opentelemetry.io/otel/exporters/otlp/otlptrace/otlptracegrpc"
+	"go.opentelemetry.io/otel/propagation"
+	"go.opentelemetry.io/otel/sdk/resource"
+	"go.opentelemetry.io/otel/sdk/trace"
+	semconv "go.opentelemetry.io/otel/semconv/v1.4.0"
 )
 
-func headers(w http.ResponseWriter, req *http.Request) {
+func newExporter(ctx context.Context) (trace.SpanExporter, error) {
+	exporter, err := otlptracegrpc.New(
+		ctx,
+		otlptracegrpc.WithInsecure(),
+		otlptracegrpc.WithEndpoint("tempo:4317"),
+	)
+	if err != nil {
+		return nil, err
+	}
 
-	for name, headers := range req.Header {
-		for _, h := range headers {
-			fmt.Fprintf(w, "%v: %v\n", name, h)
+	return exporter, nil
+}
+
+func newTraceProvider(ctx context.Context) (*trace.TracerProvider, error) {
+	traceExporter, err := newExporter(ctx)
+	if err != nil {
+		return nil, err
+	}
+
+	traceRes, err := resource.New(ctx,
+		resource.WithAttributes(semconv.ServiceNameKey.String("APP")),
+	)
+	if err != nil {
+		return nil, err
+	}
+
+	traceProvider := trace.NewTracerProvider(
+		trace.WithBatcher(traceExporter,
+			trace.WithBatchTimeout(time.Second),
+		),
+		trace.WithResource(traceRes),
+	)
+
+	return traceProvider, nil
+}
+
+func setupOTelSDK(ctx context.Context) (shutdown func(context.Context) error, err error) {
+	var shutdownFuncs []func(context.Context) error
+
+	shutdown = func(ctx context.Context) error {
+		var err error
+		for _, fn := range shutdownFuncs {
+			err = errors.Join(err, fn(ctx))
+		}
+		shutdownFuncs = nil
+		return err
+	}
+
+	handleErr := func(inErr error) {
+		err = errors.Join(inErr, shutdown(ctx))
+	}
+
+	prop := newPropagator()
+	otel.SetTextMapPropagator(prop)
+
+	tracerProvider, err := newTraceProvider(ctx)
+	if err != nil {
+		handleErr(err)
+		return
+	}
+
+	shutdownFuncs = append(shutdownFuncs, tracerProvider.Shutdown)
+	otel.SetTracerProvider(tracerProvider)
+
+	// meterProvider, err := newMeterProvider()
+	// if err != nil {
+	// 	handleErr(err)
+	// 	return
+	// }
+	// shutdownFuncs = append(shutdownFuncs, meterProvider.Shutdown)
+	// otel.SetMeterProvider(meterProvider)
+	// loggerProvider, err := newLoggerProvider()
+	// if err != nil {
+	// 	handleErr(err)
+	// 	return
+	// }
+	// shutdownFuncs = append(shutdownFuncs, loggerProvider.Shutdown)
+	// global.SetLoggerProvider(loggerProvider)
+
+	return
+}
+
+func newPropagator() propagation.TextMapPropagator {
+	return propagation.NewCompositeTextMapPropagator(
+		propagation.TraceContext{},
+		propagation.Baggage{},
+	)
+}
+
+func headers(w http.ResponseWriter, req *http.Request) {
+	headers := make([]string, 0, len(req.Header))
+
+	for k := range req.Header {
+		headers = append(headers, k)
+	}
+
+	slices.Sort(headers)
+
+	for _, h := range headers {
+		for _, h2 := range req.Header[h] {
+			fmt.Fprintf(w, "%v: %v\n", h, h2)
 		}
 	}
+
+	time.Sleep(time.Second * time.Duration(float32(rand.IntN(20))/10))
 }
 
 func main() {
-	http.HandleFunc("/", headers)
-
-	log.Println("starting http server")
-
-	err := http.ListenAndServe(":80", nil)
-	if err != nil {
+	if err := run(); err != nil {
 		panic(err)
 	}
+}
+
+func run() (err error) {
+	ctx, stop := signal.NotifyContext(context.Background(), os.Interrupt)
+	defer stop()
+
+	otelShutdown, err := setupOTelSDK(ctx)
+	if err != nil {
+		return
+	}
+
+	defer func() {
+		err = errors.Join(err, otelShutdown(context.Background()))
+	}()
+
+	srv := &http.Server{
+		Addr:         ":80",
+		BaseContext:  func(_ net.Listener) context.Context { return ctx },
+		ReadTimeout:  time.Second,
+		WriteTimeout: 10 * time.Second,
+		Handler:      newHTTPHandler(),
+	}
+	srvErr := make(chan error, 1)
+	go func() {
+		srvErr <- srv.ListenAndServe()
+	}()
+
+	select {
+	case err = <-srvErr:
+		return
+	case <-ctx.Done():
+		stop()
+	}
+
+	return srv.Shutdown(context.Background())
+}
+
+func newHTTPHandler() http.Handler {
+	mux := http.NewServeMux()
+
+	handleFunc := func(pattern string, handlerFunc func(http.ResponseWriter, *http.Request)) {
+
+		handler := otelhttp.WithRouteTag(pattern, http.HandlerFunc(handlerFunc))
+		mux.Handle(pattern, handler)
+	}
+
+	handleFunc("/test", headers)
+
+	handler := otelhttp.NewHandler(mux, "/")
+	return handler
 }
